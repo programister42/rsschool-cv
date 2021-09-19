039c84cc94a80bc197235a780b8ff6eefab86f54
Growler42 <taras_ter@icloud.com>
2021-09-20 01:52:17 +0300

    feat: implement burger menu

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,1 +1,28 @@
 "use strict";
+
+const menuButton = document.querySelector('.mini-header')
+const menu = document.querySelector('.burger-menu')
+
+if (menuButton) {
+  menuButton.addEventListener("click", function (e) {
+    document.body.classList.toggle('_lock');
+    menuButton.classList.toggle('_active')
+    menu.classList.toggle('_active')
+  })
+}
+
+const menuLinks = document.querySelectorAll('.menu-link')
+
+if (menuLinks.length > 0) {
+  menuLinks.forEach(menuLink => {
+    menuLink.addEventListener("click", onMenuLinkClick)
+  })
+
+  function onMenuLinkClick(e) {
+    if (menuButton.classList.contains('_active')) {
+      document.body.classList.remove('_lock')
+      menuButton.classList.remove('_active')
+      menu.classList.remove('_active')
+    }
+  }
+}
