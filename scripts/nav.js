00641351c3975f3f4f55231737812be49d059306
Growler42 <taras_ter@icloud.com>
2021-09-20 02:24:01 +0300

    feat: implement js scroll anchor

--- scripts/nav.js
+++ scripts/nav.js
@@ -2,23 +2,35 @@
 
 const menuButton = document.querySelector('.mini-header')
 const menu = document.querySelector('.burger-menu')
-
 if (menuButton) {
-  menuButton.addEventListener("click", function (e) {
-    document.body.classList.toggle('_lock');
+  menuButton.addEventListener("click", function () {
+    document.body.classList.toggle('_lock')
     menuButton.classList.toggle('_active')
     menu.classList.toggle('_active')
   })
 }
 
-const menuLinks = document.querySelectorAll('.menu-link')
-
-if (menuLinks.length > 0) {
-  menuLinks.forEach(menuLink => {
-    menuLink.addEventListener("click", onMenuLinkClick)
+const navLinks = document.querySelectorAll('.menu-link')
+if (navLinks.length > 0) {
+  navLinks.forEach(navLink => {
+    navLink.addEventListener("click", onMenuLinkClick)
   })
 
   function onMenuLinkClick(e) {
+    const navLink = e.target
+
+    if (navLink.dataset.goto && document.querySelector(navLink.dataset.goto)) {
+      const gotoBlock = document.querySelector(navLink.dataset.goto)
+      const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight
+
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: "smooth",
+      })
+
+      e.preventDefault();
+    }
+
     if (menuButton.classList.contains('_active')) {
       document.body.classList.remove('_lock')
       menuButton.classList.remove('_active')
